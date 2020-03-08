a196a9caa12b208e40dc5542d5b3237aa7b956f8
Adeeb Ahmad <adeebahmad0010@gmail.com>
2020-03-09 00:00:58 +0500

    Updates

---
+++ precache-manifest.77319d3423eaa5f683bb8d757f3cc8c8.js
@@ -0,0 +1,546 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8eb043b751457dc271eb3ae2cb0dfb",
+    "url": "/adeebAhmad1/Hotux.git/index.html"
+  },
+  {
+    "revision": "0ae1515b0af0bf861c01",
+    "url": "/adeebAhmad1/Hotux.git/static/css/2.cb17d3fb.chunk.css"
+  },
+  {
+    "revision": "61915aec2c566c46eb2c",
+    "url": "/adeebAhmad1/Hotux.git/static/css/main.1f9b8c03.chunk.css"
+  },
+  {
+    "revision": "0ae1515b0af0bf861c01",
+    "url": "/adeebAhmad1/Hotux.git/static/js/2.5b81f041.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/adeebAhmad1/Hotux.git/static/js/2.5b81f041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61915aec2c566c46eb2c",
+    "url": "/adeebAhmad1/Hotux.git/static/js/main.0e058a48.chunk.js"
+  },
+  {
+    "revision": "560b616046f3860aecb8",
+    "url": "/adeebAhmad1/Hotux.git/static/js/runtime-main.1ab7d5d6.js"
+  },
+  {
+    "revision": "9da2373bcac54953f58de608d0ee7ffd",
+    "url": "/adeebAhmad1/Hotux.git/static/media/404.9da2373b.jpg"
+  },
+  {
+    "revision": "3588572bca3d8f39f06dcc8a748d0b5d",
+    "url": "/adeebAhmad1/Hotux.git/static/media/404text.3588572b.jpg"
+  },
+  {
+    "revision": "9a90efe13eb4fa52c909c9c74c7dc9ba",
+    "url": "/adeebAhmad1/Hotux.git/static/media/about1.9a90efe1.jpg"
+  },
+  {
+    "revision": "cc8265f3714c83ecd6c35826729c2f2a",
+    "url": "/adeebAhmad1/Hotux.git/static/media/about2.cc8265f3.jpg"
+  },
+  {
+    "revision": "0b1183bb8fd79a0bd2f8e6a1cf4e7247",
+    "url": "/adeebAhmad1/Hotux.git/static/media/about3.0b1183bb.jpg"
+  },
+  {
+    "revision": "e6ac5bc584d857e89d0a5a8b93fd1264",
+    "url": "/adeebAhmad1/Hotux.git/static/media/about4.e6ac5bc5.jpg"
+  },
+  {
+    "revision": "9e0f936146e3f047a83c8b8531fac256",
+    "url": "/adeebAhmad1/Hotux.git/static/media/about5.9e0f9361.jpg"
+  },
+  {
+    "revision": "bb1f7878bcca5479cdb66adbd1510577",
+    "url": "/adeebAhmad1/Hotux.git/static/media/aboutus1.bb1f7878.jpg"
+  },
+  {
+    "revision": "c1f9d5bc01b84fcaee5b07f7518a3f7d",
+    "url": "/adeebAhmad1/Hotux.git/static/media/aboutus2.c1f9d5bc.jpg"
+  },
+  {
+    "revision": "fd4763dc76af6aea9fd0c892816f6a0b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/americanexpress.fd4763dc.png"
+  },
+  {
+    "revision": "ce56a143a658064d72a90f7212e67e1f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/appstore.ce56a143.png"
+  },
+  {
+    "revision": "8ff8b5e73ce38b6035c137c12dfb6074",
+    "url": "/adeebAhmad1/Hotux.git/static/media/award1.8ff8b5e7.png"
+  },
+  {
+    "revision": "39d7404a74d728e63aea228c311adf63",
+    "url": "/adeebAhmad1/Hotux.git/static/media/award2.39d7404a.png"
+  },
+  {
+    "revision": "395bbd49459c269e37fcc99a5ce373bc",
+    "url": "/adeebAhmad1/Hotux.git/static/media/award3.395bbd49.png"
+  },
+  {
+    "revision": "e6c3813c83030471c6138652ec04cc3f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/award4.e6c3813c.png"
+  },
+  {
+    "revision": "eac15a387f8da79e443b6c251234fae6",
+    "url": "/adeebAhmad1/Hotux.git/static/media/awards.eac15a38.jpg"
+  },
+  {
+    "revision": "8d2db07afb8e4e1a0cdc5198f63206be",
+    "url": "/adeebAhmad1/Hotux.git/static/media/bed-logo.8d2db07a.png"
+  },
+  {
+    "revision": "776afce42ba5760c3e65352d5308d15e",
+    "url": "/adeebAhmad1/Hotux.git/static/media/breadcrumb.776afce4.jpg"
+  },
+  {
+    "revision": "20037324c97c77ec9a2875908d64ca95",
+    "url": "/adeebAhmad1/Hotux.git/static/media/calltoaction.20037324.jpg"
+  },
+  {
+    "revision": "9c287fa33ee5a7071584377aad0bd784",
+    "url": "/adeebAhmad1/Hotux.git/static/media/comingsoonbg.9c287fa3.jpg"
+  },
+  {
+    "revision": "de0d2bf984ec00263f362a06263da9c8",
+    "url": "/adeebAhmad1/Hotux.git/static/media/counter.de0d2bf9.jpg"
+  },
+  {
+    "revision": "02d9607a5901bfea01fd06974b4dbc35",
+    "url": "/adeebAhmad1/Hotux.git/static/media/feature1.02d9607a.jpg"
+  },
+  {
+    "revision": "f85767c36d6c3cd200cca8cb00b2a985",
+    "url": "/adeebAhmad1/Hotux.git/static/media/feature2.f85767c3.jpg"
+  },
+  {
+    "revision": "58c076106190a84033ea9c0b336a2509",
+    "url": "/adeebAhmad1/Hotux.git/static/media/feature3.58c07610.jpg"
+  },
+  {
+    "revision": "ceb36c72df65089895a72422db3b05eb",
+    "url": "/adeebAhmad1/Hotux.git/static/media/feature4.ceb36c72.jpg"
+  },
+  {
+    "revision": "7b29a8a2f0072b3d2b76cbb0a9fc5597",
+    "url": "/adeebAhmad1/Hotux.git/static/media/ficon1.7b29a8a2.png"
+  },
+  {
+    "revision": "da2c6ff331237301374572dc21ae71e5",
+    "url": "/adeebAhmad1/Hotux.git/static/media/ficon2.da2c6ff3.png"
+  },
+  {
+    "revision": "af27339508dbb491f21b03940b63c9a7",
+    "url": "/adeebAhmad1/Hotux.git/static/media/ficon3.af273395.png"
+  },
+  {
+    "revision": "530e30dd11009bc3ebd4092cd8287357",
+    "url": "/adeebAhmad1/Hotux.git/static/media/ficon4.530e30dd.png"
+  },
+  {
+    "revision": "100a7e16dfdb27fcddc98993d8f08ff9",
+    "url": "/adeebAhmad1/Hotux.git/static/media/footerbg.100a7e16.jpg"
+  },
+  {
+    "revision": "98e6c2ef4a4b849deea70104dd85e645",
+    "url": "/adeebAhmad1/Hotux.git/static/media/gallerybg.98e6c2ef.jpg"
+  },
+  {
+    "revision": "6fd08752dcc3ca213ae3153ec879f540",
+    "url": "/adeebAhmad1/Hotux.git/static/media/googleplay.6fd08752.png"
+  },
+  {
+    "revision": "46c182c51eff745ccecd326030e9e96f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list1.46c182c5.jpg"
+  },
+  {
+    "revision": "738e98320e10aa430854e9f0f3a62e4f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list2.738e9832.jpg"
+  },
+  {
+    "revision": "2101f940ebc0f806c5435e18a86db5b1",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list3.2101f940.jpg"
+  },
+  {
+    "revision": "ffbaabf6a6fd5e86f6b28ba4f0079791",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list4.ffbaabf6.jpg"
+  },
+  {
+    "revision": "dcc37415e0e70b3d08078670ad8918f3",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list5.dcc37415.jpg"
+  },
+  {
+    "revision": "b608067668b5379de54abb03c3331c94",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list6.b6080676.jpg"
+  },
+  {
+    "revision": "0aff12be6480fac61774e80d4a000b94",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list7.0aff12be.jpg"
+  },
+  {
+    "revision": "4e76d35fa473d0efee446cdc5ce012f0",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list8.4e76d35f.jpg"
+  },
+  {
+    "revision": "22d1edbc9ac924a08892e235e4a2ff1b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/list9.22d1edbc.jpg"
+  },
+  {
+    "revision": "a74491cdd1416a5ab13c16a6c368ef17",
+    "url": "/adeebAhmad1/Hotux.git/static/media/loader.a74491cd.gif"
+  },
+  {
+    "revision": "0d79cd9c6e34a99fc7cc3cf61a014832",
+    "url": "/adeebAhmad1/Hotux.git/static/media/logo-black.0d79cd9c.png"
+  },
+  {
+    "revision": "f2e95cdaaf9837b8b668c85ab9b93352",
+    "url": "/adeebAhmad1/Hotux.git/static/media/logo.f2e95cda.png"
+  },
+  {
+    "revision": "dad639492450c349d26dc626f38e3ca0",
+    "url": "/adeebAhmad1/Hotux.git/static/media/mastercard.dad63949.png"
+  },
+  {
+    "revision": "75c44db7a0de4d0daa05e7b4d76f05a7",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu1.75c44db7.jpg"
+  },
+  {
+    "revision": "ef9f5b005c6745d07c14eead6466402c",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu10.ef9f5b00.jpg"
+  },
+  {
+    "revision": "158324d840d2ed3b81ff06657f961001",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu11.158324d8.jpg"
+  },
+  {
+    "revision": "aaa27cbd312f1ef43c4de92683411a1e",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu12.aaa27cbd.jpg"
+  },
+  {
+    "revision": "cb276fd64dc103b3a6e6d28cf8eb3512",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu13.cb276fd6.jpg"
+  },
+  {
+    "revision": "370297b670f0eaf3703f4c5becde59e8",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu14.370297b6.jpg"
+  },
+  {
+    "revision": "c22987cfa2f9e48d13243f5afc5965cb",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu2.c22987cf.jpg"
+  },
+  {
+    "revision": "5d80200252853b0a96d55be1a8241a83",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu3.5d802002.jpg"
+  },
+  {
+    "revision": "84feaf05f307dfab7573f145704bf908",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu4.84feaf05.jpg"
+  },
+  {
+    "revision": "3cf24dc51e6e321ab94bcb9d34a3d97f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu5.3cf24dc5.jpg"
+  },
+  {
+    "revision": "3b45fc8b41b7c9c18237c3b780afa15a",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu6.3b45fc8b.jpg"
+  },
+  {
+    "revision": "5d48d29780fe7c43060bdf0b38cfdd1d",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu7.5d48d297.jpg"
+  },
+  {
+    "revision": "26543a7694aeca34f9bb9cd8324f5a45",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu8.26543a76.jpg"
+  },
+  {
+    "revision": "330ab56d18654d5c6363c5a9f2dd6f4b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menu9.330ab56d.jpg"
+  },
+  {
+    "revision": "1607bb920efe82924e777364592481ac",
+    "url": "/adeebAhmad1/Hotux.git/static/media/menubg.1607bb92.jpg"
+  },
+  {
+    "revision": "8a17b6291d6e34a190f6314f73b7117f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news1.8a17b629.jpg"
+  },
+  {
+    "revision": "9898f7190233f53549bd45fdb5026c2f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news2.9898f719.jpg"
+  },
+  {
+    "revision": "d3bb4231cac47eba9a44719a4556b379",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news3.d3bb4231.jpg"
+  },
+  {
+    "revision": "6ca3f90cb8b3c0c00e183584c845a878",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news6.6ca3f90c.jpg"
+  },
+  {
+    "revision": "9898b3baba64fe9a3758dcd95e91dfe4",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news7.9898b3ba.jpg"
+  },
+  {
+    "revision": "bb6b7ba986d4cbe4e8931b6d21baa421",
+    "url": "/adeebAhmad1/Hotux.git/static/media/news8.bb6b7ba9.jpg"
+  },
+  {
+    "revision": "eb3fcb40df40409808e5dc6ad6c2b49c",
+    "url": "/adeebAhmad1/Hotux.git/static/media/newsletterbg.eb3fcb40.jpg"
+  },
+  {
+    "revision": "ad6769e3e8a1b609ef2e11e992a0a6b1",
+    "url": "/adeebAhmad1/Hotux.git/static/media/quote.ad6769e3.png"
+  },
+  {
+    "revision": "d7a994ae307ff2b193d5d3b0ea76f3d3",
+    "url": "/adeebAhmad1/Hotux.git/static/media/reservation.d7a994ae.jpg"
+  },
+  {
+    "revision": "620d7e809c49642a0066975a43101f86",
+    "url": "/adeebAhmad1/Hotux.git/static/media/reservationbg.620d7e80.jpg"
+  },
+  {
+    "revision": "3166d3cffa07f3d8e8b0ac79cd57f934",
+    "url": "/adeebAhmad1/Hotux.git/static/media/review1.3166d3cf.jpg"
+  },
+  {
+    "revision": "1bd2407ed89c40b6ec5e2f2e9be20e97",
+    "url": "/adeebAhmad1/Hotux.git/static/media/review2.1bd2407e.jpg"
+  },
+  {
+    "revision": "c94ee9e0af49a20355c546fd41e5ef90",
+    "url": "/adeebAhmad1/Hotux.git/static/media/reviewbg.c94ee9e0.jpg"
+  },
+  {
+    "revision": "42984918d2195c5262ad8c753db48692",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-b3.42984918.jpg"
+  },
+  {
+    "revision": "efe8e62d0c803b0fc57ac750bc2e55fa",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-1.efe8e62d.jfif"
+  },
+  {
+    "revision": "1d8cfc5ab9a8965e5fda29ff53963872",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-10.1d8cfc5a.jfif"
+  },
+  {
+    "revision": "086389e471891d98a020bf74b28b95c3",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-11.086389e4.jfif"
+  },
+  {
+    "revision": "e64af400eec42e499c71461ad4c835bd",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-12.e64af400.jfif"
+  },
+  {
+    "revision": "ecc46494065abf639a64a2b165a6d3d3",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-13.ecc46494.jfif"
+  },
+  {
+    "revision": "6be4518192f52a2d6ca972eae2e1cf1e",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-14.6be45181.jfif"
+  },
+  {
+    "revision": "e3df6d1a23a336d4a6b98c77c642b082",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-15.e3df6d1a.jfif"
+  },
+  {
+    "revision": "62094e19ce89440f01284d0acbd84b66",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-16.62094e19.jfif"
+  },
+  {
+    "revision": "04f9a23298cdf124201ae4b5139f6731",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-17.04f9a232.jfif"
+  },
+  {
+    "revision": "dc89cd802e75264ce4e3f5ed34ffc518",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-18.dc89cd80.jfif"
+  },
+  {
+    "revision": "01c98ec09f643294501388eae68d10e9",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-19.01c98ec0.jfif"
+  },
+  {
+    "revision": "e1b95fe417d195d8468be0d723b02dfd",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-2.e1b95fe4.jfif"
+  },
+  {
+    "revision": "361d9def7f6e23eff306d93ca42dabbe",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-20.361d9def.jfif"
+  },
+  {
+    "revision": "44c9308606ad30a68ac9ddcaa3f75108",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-21.44c93086.jfif"
+  },
+  {
+    "revision": "48d42c078a32619559d6bad31e0292ee",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-22.48d42c07.jfif"
+  },
+  {
+    "revision": "0b2d297d958450e274b0b6b90956ec7b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-23.0b2d297d.jfif"
+  },
+  {
+    "revision": "5480acc5a9c3c439171330151232d980",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-24.5480acc5.jfif"
+  },
+  {
+    "revision": "d8c3bf16539628e24ab371eeda30e9f7",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-25.d8c3bf16.jfif"
+  },
+  {
+    "revision": "a5924634f78c3822ee146aeddfec4033",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-3.a5924634.jfif"
+  },
+  {
+    "revision": "d489a6b2858f98707cb27704c79273f2",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-4.d489a6b2.jfif"
+  },
+  {
+    "revision": "e4e526664fe758a57c7af20d1826d3e4",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-5.e4e52666.jfif"
+  },
+  {
+    "revision": "e2ebc9fe6fbd7898268a832431258694",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-6.e2ebc9fe.jfif"
+  },
+  {
+    "revision": "2f54d4c606891896300e8a59999bad79",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-7.2f54d4c6.jfif"
+  },
+  {
+    "revision": "08c4280615fa8bd5c4d982169fbbd987",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-8.08c42806.jfif"
+  },
+  {
+    "revision": "f6cbdd1947a48ff3902870d392022b71",
+    "url": "/adeebAhmad1/Hotux.git/static/media/room-grid-9.f6cbdd19.jfif"
+  },
+  {
+    "revision": "03f24983ee76980511b6dc6a08903606",
+    "url": "/adeebAhmad1/Hotux.git/static/media/roombg.03f24983.jpg"
+  },
+  {
+    "revision": "aa22f25afd26be3d37cae287fb565890",
+    "url": "/adeebAhmad1/Hotux.git/static/media/rt1.aa22f25a.jpg"
+  },
+  {
+    "revision": "ba1abcf6fbf43ed972759463f1b9d204",
+    "url": "/adeebAhmad1/Hotux.git/static/media/rt2.ba1abcf6.jpg"
+  },
+  {
+    "revision": "f2c8b7fe6d59955dc0784235234daad8",
+    "url": "/adeebAhmad1/Hotux.git/static/media/rt3.f2c8b7fe.jpg"
+  },
+  {
+    "revision": "7e6c729124faf51ec1b25a5d845883ea",
+    "url": "/adeebAhmad1/Hotux.git/static/media/service1.7e6c7291.jpg"
+  },
+  {
+    "revision": "d9b0abb40ab4dbab9908850e5e572db7",
+    "url": "/adeebAhmad1/Hotux.git/static/media/service2.d9b0abb4.jpg"
+  },
+  {
+    "revision": "bbe85c093249d3c0672dc873ceaae0e6",
+    "url": "/adeebAhmad1/Hotux.git/static/media/service3.bbe85c09.jpg"
+  },
+  {
+    "revision": "7fdab600a1598cdcfc272e961c13ac07",
+    "url": "/adeebAhmad1/Hotux.git/static/media/sign.7fdab600.png"
+  },
+  {
+    "revision": "eb3b68f01a1394c712ed086629cb4fd8",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider1.eb3b68f0.jpg"
+  },
+  {
+    "revision": "f4e4bb9ce34652a279365b7718ea3b12",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider1.f4e4bb9c.jpg"
+  },
+  {
+    "revision": "55d5b5f3998398cac9ac29d9f5759e91",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider2.55d5b5f3.jpg"
+  },
+  {
+    "revision": "df19a9bf3d72d785bcbbf99262a9a876",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider2.df19a9bf.jpg"
+  },
+  {
+    "revision": "0c4750399b10d05d8afce49de208c355",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider3.0c475039.jpg"
+  },
+  {
+    "revision": "602229d71bee8a6d2432acf96a039601",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider3.602229d7.jpg"
+  },
+  {
+    "revision": "e635488d0998fc348707d51c7ecc4d85",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider4.e635488d.jpg"
+  },
+  {
+    "revision": "237bb0ef1b635c4f145282eb06df0d2e",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider5.237bb0ef.jpg"
+  },
+  {
+    "revision": "25927c3d2888b9d7d8112b952998d920",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider5.25927c3d.jpg"
+  },
+  {
+    "revision": "3b3e256e0a814b87fee5bde4240ed25f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider6.3b3e256e.jpg"
+  },
+  {
+    "revision": "e84d6def291d574fce74f85e8754f34b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider6.e84d6def.jpg"
+  },
+  {
+    "revision": "9ae1802559581e87e503f619202d37ea",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider7.9ae18025.jpg"
+  },
+  {
+    "revision": "b7fd1302f2ce44f7f71acda30a910c0c",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider7.b7fd1302.jpg"
+  },
+  {
+    "revision": "29f500fc9090a1bd02105759eb416d20",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider8.29f500fc.jpg"
+  },
+  {
+    "revision": "0702040c7a3167ad4fc09a1decbe5058",
+    "url": "/adeebAhmad1/Hotux.git/static/media/slider9.0702040c.jpg"
+  },
+  {
+    "revision": "41a26ae24a61e911104dd208f161b9c3",
+    "url": "/adeebAhmad1/Hotux.git/static/media/sr1.41a26ae2.jpg"
+  },
+  {
+    "revision": "2b457ee85e2bf6e4d843e7df5498266f",
+    "url": "/adeebAhmad1/Hotux.git/static/media/team1.2b457ee8.jpg"
+  },
+  {
+    "revision": "13d34ae65c5b55443b9a0a59cecda002",
+    "url": "/adeebAhmad1/Hotux.git/static/media/team2.13d34ae6.jpg"
+  },
+  {
+    "revision": "9035d34c0d9246c3cfc6de955b25985b",
+    "url": "/adeebAhmad1/Hotux.git/static/media/team3.9035d34c.jpg"
+  },
+  {
+    "revision": "e3381eb76840c6a50318f3f1ee9bae81",
+    "url": "/adeebAhmad1/Hotux.git/static/media/team4.e3381eb7.jpg"
+  },
+  {
+    "revision": "3377a294f9de57b97c0e4d5f4f55deb0",
+    "url": "/adeebAhmad1/Hotux.git/static/media/tripadvisor.3377a294.png"
+  },
+  {
+    "revision": "e828cfc56e77908fdffff933f912562a",
+    "url": "/adeebAhmad1/Hotux.git/static/media/visa.e828cfc5.png"
+  }
+]);
